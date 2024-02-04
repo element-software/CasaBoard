c1e548580e8ec4907304467eec9d253cb42f8d00
Element Software <contact@element-software.co.uk>
2024-02-04 21:39:06 +0000

    updated thermostat styling

--- src/components/Thermostat.tsx
+++ src/components/Thermostat.tsx
@@ -23,24 +23,24 @@ const Thermostat = ({ entityId }: ThermostatProps) => {
   const stateClassname = entity.attributes.running_state === "heat" ? "text-black from-yellow-600 to-amber-500" : "text-white from-neutral-800 to-neutral-700 rounded-full";
 
   return (
-    <div className="w-full p-4 text-center text-white bg-gradient-to-br from-neutral-800 to-neutral-900 rounded-2xl shadow-card shadow-neutral-800">
+    <div className="w-full p-4 text-center flex flex-col gap-2 text-white bg-gradient-to-br from-neutral-800 to-neutral-900 rounded-2xl shadow-card shadow-neutral-800">
       <div className="flex flex-row items-center justify-between">
         <div className="text-xs">{entity.attributes.friendly_name}</div>
         <Icon path={mdiPower} className={classNames("h-10 w-10 p-2 rounded-full bg-gradient-to-l", stateClassname)} aria-hidden="true" />
       </div>
-      <div className="relative w-40 h-40">
+      <div className="p-2 bg-gradient-to-l from-neutral-800 to-neutral-900 rounded-full ">
         <svg className="w-full h-full" viewBox="0 0 100 100">
           <circle
-            className="text-gray-200 stroke-current"
-            stroke-width="10"
+            className="text-neutral-800 stroke-current"
+            stroke-width="5"
             cx="50"
             cy="50"
             r="40"
             fill="transparent"
           ></circle>
           <circle
-            className="text-indigo-500 progress-ring__circle stroke-current"
-            stroke-width="10"
+            className="text-amber-500 progress-ring__circle stroke-current"
+            stroke-width="5"
             stroke-linecap="round"
             cx="50"
             cy="50"
@@ -52,11 +52,10 @@ const Thermostat = ({ entityId }: ThermostatProps) => {
           <text
             x="50"
             y="50"
-            font-family="Verdana"
             font-size="12"
             text-anchor="middle"
             alignment-baseline="middle"
-            className="text-md tracking-widest fill-white"
+            className="text-lg fill-white"
           >
               {entity.attributes.temperature}°C
           </text>
