cee9be5db6d6c2d7eee0f71fbb32fef1e07808f7
Element Software <contact@element-software.co.uk>
2024-08-13 17:04:13 +0100

    added col span to tailwind safe list

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ const config: Config = {
     { pattern: /accent-gray-/ },
     { pattern: /text-amber-/ },
     { pattern: /text-gray-/ },
+    { pattern: /col-span-/ },
   ],
   theme: {
     extend: {
