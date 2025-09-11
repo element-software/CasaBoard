b7cceef1ccaf0d5ff48e5a443ed39198cc7fec78
Element Software <contact@element-software.co.uk>
2025-09-11 09:26:06 +0100

    added gtm on public website

--- apps/public/app/layout.tsx
+++ apps/public/app/layout.tsx
@@ -1,4 +1,5 @@
-import { Analytics } from "@vercel/analytics/next"
+import { GoogleTagManager } from "@next/third-parties/google";
+import { Analytics } from "@vercel/analytics/next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -11,9 +12,9 @@ export default function PublicLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}
-        <Analytics />
-      </body>
+      <GoogleTagManager gtmId="G-4N9M4MTHP1" />
+      <Analytics />
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
