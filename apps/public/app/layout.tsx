d59fda189c44f31c3f128923848f31fcdc4965f2
Element Software <contact@element-software.co.uk>
2025-09-11 09:31:25 +0100

    added ga to public app

--- apps/public/app/layout.tsx
+++ apps/public/app/layout.tsx
@@ -1,4 +1,4 @@
-import { GoogleTagManager } from "@next/third-parties/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { Analytics } from "@vercel/analytics/next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -12,9 +12,9 @@ export default function PublicLayout({
 }) {
   return (
     <html lang="en">
-      <GoogleTagManager gtmId="G-4N9M4MTHP1" />
       <Analytics />
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId="G-4N9M4MTHP1" />
     </html>
   );
 }
