eef4d71bb967d019da4720a33ba693a11818f2d7
Element Software <contact@element-software.co.uk>
2025-09-17 18:51:03 +0100

    CB-7: added skeleton to login form

--- packages/ui/components/Login/LoginForm.tsx
+++ packages/ui/components/Login/LoginForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { SupabaseClient } from "@repo/lib";
 import { useSearchParams } from "next/navigation";
 import { CasaBoardLogo } from "@repo/ui/components/Logo/index";
