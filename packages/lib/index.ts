b755988aab8973e2819ca73fe3c00cf9eea0b24a
Element Software <contact@element-software.co.uk>
2025-09-29 23:35:26 +0100

    CB-1: removed user settings types and actions

--- packages/lib/index.ts
+++ packages/lib/index.ts
@@ -1,5 +1,4 @@
 export * as PageActions from './actions/pageActions';
-export * as UserSettingsActions from './actions/userSettingsActions';
 export * as SupabaseServer from './supabase/server';
 export * as SupabaseMiddleware from './supabase/middleware';
 export * as SupabaseClient from './supabase/client';
--- packages/types/user.ts
+++ packages/types/user.ts
@@ -1,10 +1,3 @@
-export interface UserSettings {
-  id: string;
-  user_id: string;
-  created_at: string;
-  updated_at: string | null;
-}
-
 export type Profile = {
   email: string | null;
   id: string | null;
